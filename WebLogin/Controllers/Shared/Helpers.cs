d27bb314e9a9f548ab9d662e1dd7be29a61f5700
Ignasi Carreño <ignasic@gmail.com>
2018-09-28 21:41:20 +0200

    Initial commit

---
+++ WebLogin/Controllers/Shared/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WebLogin.Controllers.Shared
+{
+    public class Helpers
+    {
+        /// <summary>
+        /// Encode given string using SHA1 algorithm
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string EncodeSHA1(string value)
+        {
+            var hash = System.Security.Cryptography.SHA1.Create();
+            var encoder = new System.Text.ASCIIEncoding();
+            var combined = encoder.GetBytes(value ?? "");
+            return BitConverter.ToString(hash.ComputeHash(combined)).ToLower().Replace("-", "");
+        }
+    }
+}
